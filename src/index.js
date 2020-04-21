d3ac5a1a7213ee107ee4e0cb3421857fa7ccb5ef
Jerome Lachaud <jerome.lachaud@gmail.com>
2020-04-21 14:10:53 +0200

    Set up basic styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './components/App'
+import './index.css'
 import store from './store'
 
 ReactDOM.render(
